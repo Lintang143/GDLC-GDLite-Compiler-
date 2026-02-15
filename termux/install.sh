bb85a59d1a70cd57da9198a4ff625014342a755c
Lintang143 <lintangsholeh860@gmail.com>
2026-02-15 08:55:34 +0700

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -1,2 +1,12 @@
-cp gdlc /data/data/com.termux/files/usr/bin
-chmod +x /data/data/com.termux/files/usr/bin/gdlc
+echo "Installing GDLite Compiler..."
+
+rm -rf /data/data/com.termux/files/home/gdlite
+git clone https://github.com/Lintang143/GDLC-GDLite-Compiler /data/data/com.termux/files/home/gdlite
+
+chmod +x /data/data/com.termux/files/home/gdlite/gdlc
+cp /data/data/com.termux/files/home/gdlite/gdlc /usr/bin
+
+rm -rf /data/data/com.termux/files/home/gdlite
+
+echo "Installation complete!"
+echo "Type: gdlite"
