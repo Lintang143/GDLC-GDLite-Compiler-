1743b35a00c41f16a26d8bf4a2b45c95b1f82551
Lintang143 <lintangsholeh860@gmail.com>
2026-02-15 10:24:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "Installing GDLite Compiler..."
 
 rm -rf /home/gdlite
-git clone https://github.com/Lintang143/GDLC-GDLite-Compiler /home/gdlite
+git clone https://github.com/Lintang143/GDLC-GDLite-Compiler- /home/gdlite
 
 chmod +x /home/gdlite/gdlc
 cp /home/gdlite/gdlc /usr/bin
