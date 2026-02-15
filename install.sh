d43eb4fb6ceeb4d7612657cbd0398fd0f3b0b978
Lintang143 <lintangsholeh860@gmail.com>
2026-02-15 08:40:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,16 @@
-cp gdlc /usr/bin
-chmod +x /usr/bin/gdlc
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Installing GDLite Compiler..."
+INSTALL_DIR="$PREFIX/bin"
+TMP_DIR="$HOME/.gdlite_tmp"
+
+rm -rf $TMP_DIR
+git clone https://github.com/Lintang143/GDLC-GDLite-Compiler $TMP_DIR
+
+chmod +x $TMP_DIR/gdlc
+cp $TMP_DIR/gdlc $INSTALL_DIR
+
+rm -rf $TMP_DIR
+
+echo "Installation complete!"
+echo "Type: gdlite"
